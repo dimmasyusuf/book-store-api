0fb11b74ed9dafbb857e39fc257fcbb701f365e1
dimmasyusuf <dimasyusufqurohman@gmail.com>
2023-10-23 17:47:50 +0700

    feat: update updateBook controller

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 router.get('/books', getAllBooks);
 router.get('/books/:id', getBookById);
 router.post('/books', createBook);
-router.put('/books/:id', updateBook);
+router.patch('/books/:id', updateBook);
 router.delete('/books/:id', deleteBook);
 
 module.exports = router;
