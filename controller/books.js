f1794c4a519d49b9f07833c74343e3b3397e89e8
dimmasyusuf <dimasyusufqurohman@gmail.com>
2023-10-23 17:42:45 +0700

    feat: update createBook, and create updateBook, deleteBook controllers

--- controller/books.js
+++ controller/books.js
@@ -45,25 +45,98 @@ const getBookById = async (req, res) => {
   }
 };
 
-const createBook = (req, res) => {
-  const { title, description, author } = req.body;
+const createBook = async (req, res) => {
+  try {
+    const { title, description, author } = req.body;
 
-  const id = books[books.length - 1].id + 1;
+    const book = await Book.create({
+      title,
+      description,
+      author,
+    });
 
-  const book = {
-    id,
-    title,
-    description,
-    author,
-  };
-
-  books.push(book);
-
-  return res.status(201).json({
-    status: 201,
-    message: 'Book created',
-    data: book,
-  });
+    return res.status(201).json({
+      status: 201,
+      message: 'Book created',
+      data: book,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+      data: {},
+    });
+  }
 };
 
-module.exports = { getAllBooks, getBookById, createBook };
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, author } = req.body;
+
+    const book = await Book.findByPk(id);
+
+    if (!book) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Book not found',
+        data: {},
+      });
+    } else {
+      const updatedBook = await book.update({
+        title,
+        description,
+        author,
+      });
+
+      return res.status(200).json({
+        status: 200,
+        message: 'Book updated',
+        data: updatedBook,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+      data: {},
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findByPk(id);
+
+    if (!book) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Book not found',
+        data: {},
+      });
+    } else {
+      await book.destroy();
+      return res.status(200).json({
+        status: 200,
+        message: 'Book deleted',
+        data: {},
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+      data: {},
+    });
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBooks, getBookById, createBook } = require('../controller/books');
+const {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+} = require('../controller/books');
 
 router.get('/', (req, res) => {
   return res.status(200).json({
@@ -13,5 +19,7 @@ router.get('/', (req, res) => {
 router.get('/books', getAllBooks);
 router.get('/books/:id', getBookById);
 router.post('/books', createBook);
+router.put('/books/:id', updateBook);
+router.delete('/books/:id', deleteBook);
 
 module.exports = router;
